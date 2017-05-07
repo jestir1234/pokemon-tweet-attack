0a1b391be23e86006d9615c0eb779f647c46c3c7
MATT <jestir1234@gmail.com>
2017-05-07 01:47:09 -0400

    got all freebeer tweets

--- app/controllers/api/twitter_controller.rb
+++ app/controllers/api/twitter_controller.rb
@@ -1,13 +1,22 @@
 class Api::TwitterController < ApplicationController
 
   def index
-    debugger
+
     client = Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['consumer_api_key']
       config.consumer_secret = ENV['consumer_api_secret']
       config.access_token = ENV['access_token']
       config.access_token_secret = ENV['access_token_secret']
     end
-    debugger
+
+    search_options = {
+      result_type: "recent",
+      # geocode: "40.730610,-73.935242,10mi"
+    }
+
+    search = client.search("#freebeer -rt").take(10).each do |tweet|
+        debugger
+    end
+
   end
 end
