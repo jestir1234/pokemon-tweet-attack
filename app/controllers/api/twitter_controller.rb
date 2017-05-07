cb5a668dd0a84732e80d56ff8ab3a7d6c35b8296
MATT <jestir1234@gmail.com>
2017-05-07 00:24:32 -0400

    can get tweets

--- app/controllers/api/twitter_controller.rb
+++ app/controllers/api/twitter_controller.rb
@@ -5,6 +5,9 @@ class Api::TwitterController < ApplicationController
     client = Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['consumer_api_key']
       config.consumer_secret = ENV['consumer_api_secret']
+      config.access_token = ENV['access_token']
+      config.access_token_secret = ENV['access_token_secret']
     end
+    debugger
   end
 end
