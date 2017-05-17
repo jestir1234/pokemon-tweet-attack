2b9d5156a45dc9bbd46a5ccc28703bf8d8bd0706
MATT <jestir1234@gmail.com>
2017-05-17 11:35:02 -0400

    begin heroku setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require("path");
+const webpack = require("webpack");
+const prod = process.argv.indexOf('-p') !== -1;
+
+module.exports = {
+  context: __dirname,
+  entry: "./app/views/static_pages/index.html.erb",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        NODE_ENV:JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+};
